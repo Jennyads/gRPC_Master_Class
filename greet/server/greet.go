27abbdb43d3aa912d19ecfb6e0eb0b5fcca56641
Jennyads <jennycristina.1998@gmail.com>
2024-03-04 15:44:50 -0300

    added server and client greet

---
+++ greet/server/greet.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"context"
+	pb "github.com/Jennyads/gRPC_Master_Class/greet/proto"
+	"log"
+)
+
+func (s *Server) Greet(ctx context.Context, in *pb.GreetRequest) (*pb.GreetResponse, error) {
+	log.Printf("Greet function was invoked with %v\n", in)
+	return &pb.GreetResponse{Result: "Hello " + in.FirstName}, nil
+}
